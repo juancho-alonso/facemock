e86875752dd6b18a88db6e4fda6f549073a90557
juancho-alonso <juan.agustin.alonso@gmail.com>
2020-08-04 15:39:12 +1000

    updated project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InitialComponent } from './initial/initial.component';
+import { WallComponent } from './wall/wall.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InitialComponent,
+    WallComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule
