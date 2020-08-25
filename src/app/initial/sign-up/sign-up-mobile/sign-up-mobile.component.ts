07dc915f3e82d8f7ea59732330d9f3a2e2c128bd
juancho-alonso <juan.agustin.alonso@gmail.com>
2020-08-25 15:40:40 +1000

    FMK-012 #comment Building routing logic for navigation on desktop and mobile on first screen

---
+++ src/app/initial/sign-up/sign-up-mobile/sign-up-mobile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up-mobile',
+  templateUrl: './sign-up-mobile.component.html',
+  styleUrls: ['./sign-up-mobile.component.scss']
+})
+export class SignUpMobileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
