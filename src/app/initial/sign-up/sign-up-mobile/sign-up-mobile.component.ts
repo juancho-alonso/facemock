2dce6e7e49848f5b5fe18ecbec7b3883408a481b
juancho-alonso <juan.agustin.alonso@gmail.com>
2020-08-27 20:50:01 +1000

    FMK-004 #comment Adding form validation for sign up pages on mobile

--- src/app/initial/sign-up/sign-up-mobile/sign-up-mobile.component.ts
+++ src/app/initial/sign-up/sign-up-mobile/sign-up-mobile.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-sign-up-mobile',
@@ -7,9 +9,92 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpMobileComponent implements OnInit {
 
+  @ViewChild('formNames') formNames: NgForm;
+  @ViewChild('formDate') formDate: NgForm;
+
+
+  namesOk = 1;
+  days = [];
+  months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+            'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  years = [];
+  pronouns = [{
+    subject: 'She',
+    object: 'her'
+  },
+  {
+    subject: 'He',
+    object: 'him'
+  },
+  {
+    subject: 'They',
+    object: 'them'
+  }  
+]
+
+genders = ['Female', 'Male']
+
+  defaultDay = "day";
+  defaultMonth = "month";
+  defaultYear = "year";
+  defaultPronoun = "pronoun"; 
+
+customGender = true;
+displayGender = false;
+
   constructor() { }
 
   ngOnInit(): void {
+    for (let i = 1; i < 32; i++){
+      this.days.push(i);
+    };
+
+    for (let j = 2020; j > 1900; j--){
+      this.years.push(j);
+    }
   }
 
+  onNextReg(){
+      var nameInputs = document.querySelectorAll<HTMLInputElement>(".input-names");
+      var namesArr = Array.from(nameInputs)
+      var dateInputs = document.querySelectorAll<HTMLSelectElement>(".date-input")
+      var dateArr = Array.from(dateInputs);
+      var genderInputs = document.querySelectorAll<HTMLElement>(".gender-input");
+      var genderArr = Array.from(genderInputs);
+      console.log(genderArr[2]);
+
+      for (const cur of namesArr) {
+        cur.style.borderColor = "1px solid lightgray";
+        if (cur.className.includes('ng-invalid') && !this.formNames.valid){
+          cur.style.border = "1px solid red"
+          this.namesOk = 1;
+      } else if (cur.className.includes('ng-valid') && this.formNames.valid){
+          cur.style.borderColor = "1px solid lightgray";
+          this.namesOk = 2;
+      } 
+      }
+      
+      for (const cur of dateArr){
+        cur.style.borderColor = "1px solid lightgray";
+        if (cur.value != "day" && cur.value != "month" && cur.value != "year"){
+          console.log(cur.value + ' IF')
+          cur.style.border = "1px solid lightgray"
+          this.namesOk = 3;
+      } else if (cur.value === "day" || "month" || "year"){
+            console.log(cur.value + ' IF ELSE')
+          cur.style.border = "1px solid red";
+          this.namesOk = 2;
+      } 
+      }
+      
+  }
+
+  onCustomChange() {
+    this.customGender = !this.customGender;
+    this.displayGender = !this.displayGender;
+  }
+
+  onSubmit(form: NgForm){
+    console.log(form);
+  }
 }
